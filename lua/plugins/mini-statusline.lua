d08c857de1a3dce0a79745290a8269ee9f936ccd
Jonathan Pinilla <jonahan@jonathanpf.dev>
2026-02-01 10:17:22 -0500

    Setup files, tabline and trailspace mini plugins

--- lua/plugins/mini-statusline.lua
+++ lua/plugins/mini-statusline.lua
@@ -1,10 +1,22 @@
 return {
-	{
-		'echasnovski/mini.nvim',
-		enabled = true,
-		config = function()
-			local statusline = require 'mini.statusline'
-			statusline.setup { use_icons = true }
-		end
-	},
+  {
+    'echasnovski/mini.nvim',
+    enabled = true,
+    config = function()
+      local statusline = require 'mini.statusline'
+      local trailspace = require 'mini.trailspace'
+      local files = require 'mini.files'
+      local icons = require 'mini.icons'
+      local tabline = require 'mini.tabline'
+      icons.setup{}
+      files.setup{
+        vim.keymap.set("n", "<leader>e", function () files.open() end, { desc = "Trim trailspaces" } )
+      }
+      tabline.setup{}
+      statusline.setup { use_icons = true }
+      trailspace.setup{
+        vim.keymap.set("n", "<leader>tw", function () trailspace.trim() end, { desc = "Trim trailspaces" } )
+      }
+    end
+  },
 }
